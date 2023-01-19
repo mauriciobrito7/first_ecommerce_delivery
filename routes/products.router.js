c67e259769d50354aeeb1a2f2a4a7fc8324b0ff1
Mauricio Brito <maubot01@gmail.com>
2023-01-19 20:36:08 -0300

    fix: code formatted

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,69 +1,69 @@
 import { Router } from 'express';
-import { ProductManager } from '../models/productManager.js';
+import ProductManager from '../models/productManager.js';
 import { __dirname } from '../utils.js';
 
 const router = Router();
 const productManager = new ProductManager(`${__dirname}/db/products.json`);
 
 router.get('/', async(req, res) => {
- const products = await productManager.getProducts();
- res.json(products);
+  const products = await productManager.getProducts();
+  res.json(products);
 });
 
 router.get('/:id', async(req, res) => {
- const id = parseInt(req.params.id);
- const product = await productManager.getProductById(id);
- res.json(product);
+  const id = parseInt(req.params.id);
+  const product = await productManager.getProductById(id);
+  res.json(product);
 });
 
 router.post('/', async(req, res) => {
- const body = req.body;
- const { title, description, price, thumbnail, code, stock } = body;
- const product = await productManager.addProduct(
-  title,
-  description,
-  price,
-  thumbnail,
-  code,
-  stock
- );
+  const body = req.body;
+  const { title, description, price, thumbnail, code, stock } = body;
+  const product = await productManager.addProduct(
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock
+  );
 
- if(product) {
-  res.json({ message:'product added successfully', product});
- } else {
-  res.status(400).json({ message:'product not added' });
- }
+  if(product) {
+    res.json({ message:'product added successfully', product});
+  } else {
+    res.status(400).json({ message:'product not added' });
+  }
 });
 
 router.put('/:id', async(req, res) => {
- const id = parseInt(req.params.id);
- const body = req.body;
- const { title, description, price, thumbnail, code, stock } = body;
- const product = await productManager.updateProduct(
+  const id = parseInt(req.params.id);
+  const body = req.body;
+  const { title, description, price, thumbnail, code, stock } = body;
+  const product = await productManager.updateProduct(
   id,
   {
-   title,
-   description,
-   price,
-   thumbnail,
-   code,
-   stock
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock
   });
   if(product) {
-   res.json({ message:'product updated successfully', product});
+    res.json({ message:'product updated successfully', product});
   } else {
-   res.status(400).json({ message:'product not updated' });
+    res.status(400).json({ message:'product not updated' });
   }
 });
 
 router.delete('/:id', async (req, res) => {
- const id = parseInt(req.params.id);
- const products = await productManager.deleteProduct(id);
- if(products) {
+  const id = parseInt(req.params.id);
+  const products = await productManager.deleteProduct(id);
+  if(products) {
   res.json({ message:'product deleted successfully', products});
- } else {
-  res.status(400).json({ message:'product not deleted' });
- }
+  } else {
+    res.status(400).json({ message:'product not deleted' });
+  }
 })
 
 export default router;
